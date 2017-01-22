53c7fb78dfdfea2823ca53166fcd601514f32e22
Piffek <patrykpiwko123412@gmail.com>
2017-01-22 09:40:45 +0100

    add views and model

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 ini_set('display_errors', '1');
 error_reporting(E_STRICT | E_ALL ^ E_NOTICE);
 
-require 'app/Router.php';
-require 'app/Controller.php';
+require_once 'app/Router.php';
+require_once 'app/Controller.php';
+require_once 'app/Model.php';
+require_once 'app/View.php';
 
 $router = new Router();
